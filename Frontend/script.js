5130a9eb7866cc93ba46c1d9396599d465040a00
Mayur Sawant <sawantmayur66@gmail.com>
2023-09-10 11:24:33 +0530

    Update script.js

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,16 +1,20 @@
+// Adding an event listener to the DOMContentLoaded event to execute the anonymous function once the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', () => {
+    // Getting references to various DOM elements
     const searchBar = document.getElementById('search-bar');
     const searchButton = document.getElementById('search-button');
     const intervalSelect = document.getElementById('interval');
     const indicatorSelect = document.getElementById('indicator-select');
     const overlayCheckbox = document.getElementById('overlay');
     const getIndicatorChart = document.getElementById('get-indicator-chart');
-
+    
+    // Getting reference to the chart containers and initializing chart variables
     const chartContainer = document.getElementById('chart-container');
     let chart;
-    let indicatorChart;  // for the indicator chart
+    let indicatorChart;
     const indicatorChartContainer = document.getElementById('indicator-chart-container');
-
+    
+    // Function to fetch data from the specified URL using fetch API
     async function fetchData(url) {
         try {
             const response = await fetch(url);
@@ -23,6 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    // Function to create a stock chart using Highcharts library
     function createChart(data) {
         chart = Highcharts.stockChart(chartContainer, {
             rangeSelector: {
@@ -42,22 +47,24 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-   function createIndicatorChart(data, seriesName = 'Indicator') {
-       if(indicatorChart) {
-           indicatorChart.addSeries({
-               name: seriesName,
-               data: data,
-               tooltip: {
-                   valueDecimals: 2
-               },
-               color: 'red',
-               dashStyle: 'longdash',
-           });
-       } else {
-           console.error('No indicator chart to overlay data onto');
-       }
-   }
+    // Function to create or update the indicator chart by adding a new series to it
+    function createIndicatorChart(data, seriesName = 'Indicator') {
+        if(indicatorChart) {
+            indicatorChart.addSeries({
+                name: seriesName,
+                data: data,
+                tooltip: {
+                    valueDecimals: 2
+                },
+                color: 'red',
+                dashStyle: 'longdash',
+            });
+        } else {
+            console.error('No indicator chart to overlay data onto');
+        }
+    }
 
+    // Function to create a separate indicator chart
     function createSeparateIndicatorChart(data, seriesName = 'Indicator') {
         indicatorChart = Highcharts.chart(indicatorChartContainer, {
             title: {
@@ -78,6 +85,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    // Function to create an HTML table to display the indicator data
     function createIndicatorTable(data) {
         const tableContainer = document.getElementById('table-container');
         tableContainer.innerHTML = '';
@@ -110,8 +118,8 @@ document.addEventListener('DOMContentLoaded', () => {
             tableContainer.innerText = 'No data available to display';
         }
     }
-    
-    
+
+    // Adding an event listener to the search button click event to fetch and display data based on the selected symbol and interval
     searchButton.addEventListener('click', async () => {
         const symbol = searchBar.value;
         const interval = intervalSelect.value;
@@ -123,7 +131,7 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             console.error('No data available to plot the chart');
         }
-     
+
         const indicatorData = await fetchData(`http://3.84.81.214:5000/indicator_table/${symbol}/${encodeURIComponent(interval)}/${indicator}`);
 
         if(indicatorData && indicatorData.length > 0) {
@@ -134,25 +142,25 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    
+    // Adding an event listener to the get indicator chart button click event to fetch and display the indicator chart based on the selected symbol, interval, and indicator
     getIndicatorChart.addEventListener('click', async () => {
-    const symbol = searchBar.value;
-    const interval = intervalSelect.value;
-    const indicator = indicatorSelect.value;
+        const symbol = searchBar.value;
+        const interval = intervalSelect.value;
+        const indicator = indicatorSelect.value;
 
-    const indicatorData = await fetchData(`http://3.84.81.214:5000/indicator_plot/${symbol}/${encodeURIComponent(interval)}/${indicator}`);
-    
-    const messageElement = document.getElementById('message');
-    
-    if(indicatorData && indicatorData.length > 0) {
-        if (overlayCheckbox.checked) {
-            createIndicatorChart(indicatorData, indicator.toUpperCase());
+        const indicatorData = await fetchData(`http://3.84.81.214:5000/indicator_plot/${symbol}/${encodeURIComponent(interval)}/${indicator}`);
+
+        const messageElement = document.getElementById('message');
+
+        if(indicatorData && indicatorData.length > 0) {
+            if (overlayCheckbox.checked) {
+                createIndicatorChart(indicatorData, indicator.toUpperCase());
+            } else {
+                createSeparateIndicatorChart(indicatorData, indicator.toUpperCase());
+            }
+            messageElement.innerText = '';
         } else {
-            createSeparateIndicatorChart(indicatorData, indicator.toUpperCase());
-        }
-        messageElement.innerText = '';
-    } else {
-         messageElement.innerText = 'Enough data points not available to plot the indicator';
+             messageElement.innerText = 'Enough data points not available to plot the indicator';
         }
     });
 });
